2cda5589bd5fce9d85fc16412bc5174c6f1d3d56
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2020-01-01 21:07:04 -0500

    a ton of stuff i forgot to commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    const title = 'Welcome!';
+    res.render('index', {
+        title: title
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+        .sort({date: 'desc'})
+        .then(ideas => {
+            res.render('ideas/index', { ideas: ideas})
+        })
+
+});
+
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add')
+});
+
+app.post('/ideas', (req, res) => {
+    let errors = [];
+
+    if (!req.body.title) {
+        errors.push({ text: 'Please add a title' })
+    }
+    if (!req.body.details) {
+        errors.push({ text: 'Please add some details' })
+    }
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            })
+    }
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
